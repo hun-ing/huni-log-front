8865ef40265e3642a6def5f09673af737963145d
yonghun.jeong <jeong.yonghun90@gmail.com>
2024-04-28 21:46:10 +0900

    feat: 뷰어
    - Intersection Observer API 제목 트리거 옵션 조정

--- pages/viewer/index.vue
+++ pages/viewer/index.vue
@@ -18,17 +18,33 @@ watch(x, (value) => {
 })
 
 onMounted(() => {
+  const viewer = document.querySelector('#viewer')
   Editor.factory({
-    el: document.querySelector('#viewer'),
+    el: viewer,
     viewer: true,
     height: '500px',
     initialValue: markdownContent,
+  })
+
+  function callback(entries, observer) {
+    console.log('callback!!')
+  }
+
+  const options = {
+    root: null,
+    rootMargin: '0px 0px -95% 0px',
+    threshold: 1.0,
+  }
+
+  viewer.querySelectorAll('h3').forEach((item) => {
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(item)
   })
 })
 </script>
 
 <template>
-  <v-row ref="rowEl" class="h-100">
+  <v-row id="test" ref="rowEl" class="h-100">
     <v-list ref="listEl" min-width="200" class="position-fixed bg-transparent" :style="{ right: `${listRight}px` }" style="border-left: 2px solid #F1F3F5;">
       <v-list-item
         v-for="n in 3"
