06b7d935c237a44d4f5069eec548ca75ea15ba8b
yonghun.jeong <jeong.yonghun90@gmail.com>
2024-04-29 19:47:44 +0900

    feat: 뷰어
    - title navigation 옵저버 구현

--- pages/viewer/index.vue
+++ pages/viewer/index.vue
@@ -9,12 +9,13 @@ import { markdownContent } from '~/assets/markdownContent.js'
 const listRight = ref(0)
 const rowEl = ref(null)
 const listEl = ref(null)
+const titleList = ref([])
 
 const { x } = useElementBounding(rowEl)
 const { width } = useElementBounding(listEl)
 
 watch(x, (value) => {
-  listRight.value = value - width.value - 30
+  listRight.value = value - width.value - 60
 })
 
 onMounted(() => {
@@ -22,21 +23,28 @@ onMounted(() => {
   Editor.factory({
     el: viewer,
     viewer: true,
-    height: '500px',
     initialValue: markdownContent,
   })
 
   function callback(entries, observer) {
-    console.log('callback!!')
+    entries.forEach((entry) => {
+      titleList.value.forEach(item => item.isSelected = item.id === entry.target.id)
+    })
   }
 
   const options = {
     root: null,
-    rootMargin: '0px 0px -95% 0px',
-    threshold: 1.0,
+    rootMargin: '0px 0px -97% 0px',
+    threshold: 0,
   }
 
-  viewer.querySelectorAll('h3').forEach((item) => {
+  viewer.querySelectorAll('h3').forEach((item, index) => {
+    titleList.value.push({
+      id: `title-${index}`,
+      title: item.textContent,
+      isSelected: false,
+    })
+    item.id = `title-${index}`
     const observer = new IntersectionObserver(callback, options)
     observer.observe(item)
   })
@@ -44,13 +52,18 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-row id="test" ref="rowEl" class="h-100">
+  <v-row ref="rowEl" class="h-100">
     <v-list ref="listEl" min-width="200" class="position-fixed bg-transparent" :style="{ right: `${listRight}px` }" style="border-left: 2px solid #F1F3F5;">
       <v-list-item
-        v-for="n in 3"
-        :key="n"
-        :title="`태그 ${n}`"
-      />
+        v-for="item in titleList"
+        :key="item.id"
+        tag="li"
+        min-height="auto"
+      >
+        <template #subtitle>
+          <a :href="`#${item.id}`" class="text-decoration-none" :class="item.isSelected ? 'font-weight-bold' : ''">{{ item.title }}</a>
+        </template>
+      </v-list-item>
     </v-list>
     <v-col cols="12">
       <div id="viewer" />
@@ -59,4 +72,8 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.text-decoration-none {
+  text-decoration: none !important;
+  color: inherit;
+}
 </style>
